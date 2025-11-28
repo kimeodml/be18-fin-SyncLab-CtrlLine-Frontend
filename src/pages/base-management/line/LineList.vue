643cd499ea3fd31452dfd10afae3092e89c7b241
kimeodml <kiemdoml@gmail.com>
2025-11-28 09:29:45 +0000

    [Feat] 라인 목록조회 상세조회 (#42)
    * [feat] query-functions 추가
    
    * [feat] query-hooks 작성.
    
    * [refactor] query-hooks 작성.
    
    * [feat] 상세조회 업데이트 query-hooks 작성
    
    * [feat] 상세조회 useGetLine.js
    
    * [feat] 라인 FilterTab 생성
    
    * [feat] 라인 목록 화면 구현
    
    * [feat] 라인 목록 조회 화면
    
    * [feat] 라인 상세조회 화면
    
    * [refactor] 페이지 그 기능
    
    * [feat] 목록화면에서  뒤로 가면, 그그 페이지로 가여.

--- src/pages/base-management/line/LineList.vue
+++ src/pages/base-management/line/LineList.vue
@@ -1,7 +1,120 @@
 <template>
-  <div>라인</div>
+  <div class="flex justify-between items-center">
+    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">라인 목록</h3>
+  </div>
+
+  <FilterTab :filters="filters" @search="onSearch" />
+
+  <div class="flex flex-col">
+    <div class="min-h-[600px] flex-1">
+      <Table class="w-full table-fixed">
+        <TableHeader class="border-b-2 border-primary">
+          <TableRow>
+            <TableHead class="text-center"><Checkbox /></TableHead>
+            <TableHead class="text-center">라인코드</TableHead>
+            <TableHead class="text-center">라인명</TableHead>
+            <TableHead class="text-center">담당부서</TableHead>
+            <TableHead class="text-center">담당자</TableHead>
+            <TableHead class="text-center">사용여부</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody v-if="lineList && lineList.content">
+          <TableRow
+            v-for="(line, index) in lineList.content"
+            :key="index"
+            class="hover:bg-gray-50 hover:font-medium hover:underline text-center transition-all border-b border-dotted border-gray-300 cursor-pointer"
+            @click="goToDetail(line.lineCode)"
+          >
+            <TableCell class="table-checkbox-cell py-3 whitespace-nowrap" @click.stop>
+              <Checkbox />
+            </TableCell>
+            <TableCell class="whitespace-nowrap overflow-hidden text-ellipsis">
+              {{ line.lineCode }}
+            </TableCell>
+            <TableCell class="whitespace-nowrap overflow-hidden text-ellipsis">
+              {{ line.lineName }}
+            </TableCell>
+            <TableCell class="whitespace-nowrap overflow-hidden text-ellipsis">
+              {{ line.userDepartment }}
+            </TableCell>
+            <TableCell class="whitespace-nowrap overflow-hidden text-ellipsis">
+              {{ line.userName }}
+            </TableCell>
+            <TableCell class="whitespace-nowrap overflow-hidden text-ellipsis">
+              <Badge
+                class="w-[50px] mx-auto"
+                :class="
+                  line.isActive
+                    ? 'bg-green-100 text-green-700 border-green-300'
+                    : 'bg-red-100 text-red-700 border-red-300'
+                "
+              >
+                {{ line.isActive ? '사용' : '미사용' }}
+              </Badge>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+    <BasePagination v-model="page" :total-pages="lineList?.pageInfo?.totalPages ?? 1" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+import useGetLineList from '@/apis/query-hooks/line/useGetLineList';
+import BasePagination from '@/components/pagination/BasePagination.vue';
+import { Badge } from '@/components/ui/badge';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import FilterTab from '@/pages/base-management/line/FilterTab.vue';
+import { buildQueryObject } from '@/utils/buildQueryObject';
+
+const route = useRoute();
+const router = useRouter();
+
+const onSearch = newFilters => {
+  Object.assign(filters, newFilters);
+  page.value = 1;
+  refetch();
+};
+
+const goToDetail = lineCode => {
+  router.push(`/base-management/lines/${lineCode}`);
+};
+
+const { data: lineList, refetch, page, filters } = useGetLineList();
+
+if (route.query.page) {
+  const p = Number(route.query.page);
+  if (!Number.isNaN(p) && p > 0) {
+    page.value = p;
+  }
+}
+
+const syncQuery = () => {
+  const query = buildQueryObject({
+    page: page.value,
+    ...filters,
+  });
+
+  router.replace({ query });
+};
+
+// page / status 변경 시
+watch([page], () => {
+  syncQuery();
+});
+</script>
 
 <style lang="scss" scoped></style>
